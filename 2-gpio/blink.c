83f4fb3e1fd21186543af95a18492542eb6b5d41
Kristobal <junta.kristobal@gmail.com>
2015-10-21 21:27:00 +0300

    Removed extra brace in blink.c so it compiles now Fixed indentation in blink.c

--- 2-gpio/blink.c
+++ 2-gpio/blink.c
@@ -20,75 +20,75 @@
 
 //define the 8 leds
 
-#define PIN0 RPI_V2_GPIO_P1_07		//4
-#define PIN1 RPI_V2_GPIO_P1_08		//14
-#define PIN2 RPI_V2_GPIO_P1_18		//24
-#define PIN3 RPI_V2_GPIO_P1_16		//23
-#define PIN4 RPI_V2_GPIO_P1_15		//22
-#define PIN5 RPI_V2_GPIO_P1_13		//27
-#define PIN6 RPI_V2_GPIO_P1_12		//18
-#define PIN7 RPI_V2_GPIO_P1_11		//17
+#define PIN0 RPI_V2_GPIO_P1_07      //4
+#define PIN1 RPI_V2_GPIO_P1_08      //14
+#define PIN2 RPI_V2_GPIO_P1_18      //24
+#define PIN3 RPI_V2_GPIO_P1_16      //23
+#define PIN4 RPI_V2_GPIO_P1_15      //22
+#define PIN5 RPI_V2_GPIO_P1_13      //27
+#define PIN6 RPI_V2_GPIO_P1_12      //18
+#define PIN7 RPI_V2_GPIO_P1_11      //17
 
 int main(int argc, char **argv)
 {
 
-	int i;
+    int i;
 
-	if (!bcm2835_init())
-		return 1;
+    if (!bcm2835_init())
+        return 1;
 
-	// Set the pins to be an output
-    	bcm2835_gpio_fsel(PIN0, BCM2835_GPIO_FSEL_OUTP);
-	bcm2835_gpio_fsel(PIN1, BCM2835_GPIO_FSEL_OUTP);
- 	bcm2835_gpio_fsel(PIN2, BCM2835_GPIO_FSEL_OUTP);
- 	bcm2835_gpio_fsel(PIN3, BCM2835_GPIO_FSEL_OUTP);
-    	bcm2835_gpio_fsel(PIN4, BCM2835_GPIO_FSEL_OUTP);
- 	bcm2835_gpio_fsel(PIN5, BCM2835_GPIO_FSEL_OUTP);
- 	bcm2835_gpio_fsel(PIN6, BCM2835_GPIO_FSEL_OUTP);
- 	bcm2835_gpio_fsel(PIN7, BCM2835_GPIO_FSEL_OUTP);
+    // Set the pins to be an output
+    bcm2835_gpio_fsel(PIN0, BCM2835_GPIO_FSEL_OUTP);
+    bcm2835_gpio_fsel(PIN1, BCM2835_GPIO_FSEL_OUTP);
+    bcm2835_gpio_fsel(PIN2, BCM2835_GPIO_FSEL_OUTP);
+    bcm2835_gpio_fsel(PIN3, BCM2835_GPIO_FSEL_OUTP);
+    bcm2835_gpio_fsel(PIN4, BCM2835_GPIO_FSEL_OUTP);
+    bcm2835_gpio_fsel(PIN5, BCM2835_GPIO_FSEL_OUTP);
+    bcm2835_gpio_fsel(PIN6, BCM2835_GPIO_FSEL_OUTP);
+    bcm2835_gpio_fsel(PIN7, BCM2835_GPIO_FSEL_OUTP);
 
-	while(1)
-	{
-		bcm2835_gpio_set(PIN0);
-		bcm2835_delay(500);
-		bcm2835_gpio_clr(PIN0);
-                bcm2835_delay(500);
+    while(1)
+    {
+        bcm2835_gpio_set(PIN0);
+        bcm2835_delay(500);
+        bcm2835_gpio_clr(PIN0);
+        bcm2835_delay(500);
 
-                bcm2835_gpio_set(PIN1);
-                bcm2835_delay(500);
-                bcm2835_gpio_clr(PIN1);
-                bcm2835_delay(500);
-                bcm2835_gpio_set(PIN2);
-                bcm2835_delay(500);
-                bcm2835_gpio_clr(PIN2);
-                bcm2835_delay(500);
+        bcm2835_gpio_set(PIN1);
+        bcm2835_delay(500);
+        bcm2835_gpio_clr(PIN1);
+        bcm2835_delay(500);
+        bcm2835_gpio_set(PIN2);
+        bcm2835_delay(500);
+        bcm2835_gpio_clr(PIN2);
+        bcm2835_delay(500);
 
-                bcm2835_gpio_set(PIN3);
-                bcm2835_delay(500);
-                bcm2835_gpio_clr(PIN3);
-                bcm2835_delay(500);
+        bcm2835_gpio_set(PIN3);
+        bcm2835_delay(500);
+        bcm2835_gpio_clr(PIN3);
+        bcm2835_delay(500);
 
-                bcm2835_gpio_set(PIN4);
-                bcm2835_delay(500);
-                bcm2835_gpio_clr(PIN4);
-                bcm2835_delay(500);
-		
-		bcm2835_gpio_set(PIN5);
-                bcm2835_delay(500);
-                bcm2835_gpio_clr(PIN5);
-                bcm2835_delay(500);
+        bcm2835_gpio_set(PIN4);
+        bcm2835_delay(500);
+        bcm2835_gpio_clr(PIN4);
+        bcm2835_delay(500);
 
-                bcm2835_gpio_set(PIN6);
-                bcm2835_delay(500);
-                bcm2835_gpio_clr(PIN6);
-                bcm2835_delay(500);
+        bcm2835_gpio_set(PIN5);
+        bcm2835_delay(500);
+        bcm2835_gpio_clr(PIN5);
+        bcm2835_delay(500);
 
-                bcm2835_gpio_set(PIN7);
-                bcm2835_delay(500);
-                bcm2835_gpio_clr(PIN7);
-                bcm2835_delay(500);
-	}
+        bcm2835_gpio_set(PIN6);
+        bcm2835_delay(500);
+        bcm2835_gpio_clr(PIN6);
+        bcm2835_delay(500);
+
+        bcm2835_gpio_set(PIN7);
+        bcm2835_delay(500);
+        bcm2835_gpio_clr(PIN7);
+        bcm2835_delay(500);
     }
+
     bcm2835_close();
     return 0;
 }
